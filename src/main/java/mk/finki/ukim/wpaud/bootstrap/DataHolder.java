ae437698a8855eee917c10f164edb9cdb6815e45
SaraMisajlovska <smisajlovska@gmail.com>
2021-11-03 20:59:27 +0100

    menadziranje sesija

--- src/main/java/mk/finki/ukim/wpaud/bootstrap/DataHolder.java
+++ src/main/java/mk/finki/ukim/wpaud/bootstrap/DataHolder.java
@@ -1,6 +1,7 @@
 package mk.finki.ukim.wpaud.bootstrap;
 
 import mk.finki.ukim.wpaud.model.Category;
+import mk.finki.ukim.wpaud.model.User;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -10,10 +11,14 @@ import java.util.List;
 @Component
 public class DataHolder {
     public static List<Category> categories = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
 
     @PostConstruct
     public void init() {
         categories.add(new Category("Sport", "Sport category"));
         categories.add(new Category("Software", "Software category"));
+
+        users.add(new User("sara.m", "sm", "sara", "misajlovska"));
+        users.add(new User("mara.m", "sm", "mara", "mararara"));
     }
 }
