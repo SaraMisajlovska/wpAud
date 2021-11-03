68846aa56a0e098a057dfa60f04825a84d106146
SaraMisajlovska <smisajlovska@gmail.com>
2021-11-04 00:24:08 +0100

    login filter added; css fixed

---
+++ src/main/java/mk/finki/ukim/wpaud/web/servlet/filter/LoginFilter.java
@@ -0,0 +1,41 @@
+package mk.finki.ukim.wpaud.web.servlet.filter;
+
+import mk.finki.ukim.wpaud.model.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+@WebFilter
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        User user = (User) request.getSession().getAttribute("user");
+
+        String path = request.getServletPath();
+
+        if (!path.equals("/login") && !path.equals("/main.css") && user == null){
+            response.sendRedirect("/login");
+        }
+        else {
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
+
+    }
+}
